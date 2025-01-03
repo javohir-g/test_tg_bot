ce64baab8c632631e55cf05b72f656f6af023b3e
javohir-g <807gjb@gmail.com>
2025-01-03 23:11:14 +0500

    Update request_to_site.py
    1

--- request_to_site.py
+++ request_to_site.py
@@ -5,7 +5,7 @@ import time
 
 def update_website():
     try:
-        response = requests.get("https://test-tg-bot-je8c.onrender.com")
+        response = requests.get("https://test-tg-bot-ctme.onrender.com")
         if response.status_code == 200:
             print("Сайт успешно обновлен")
         else:
